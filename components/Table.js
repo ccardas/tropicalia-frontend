0c34d024ed3a71e8f737fdd756e1778227ef4fc7
thecristian1409 <criscardas@gmail.com>
2021-06-12 17:54:12 +0200

    Table operations on mock data

--- components/Table.js
+++ components/Table.js
@@ -1,141 +1,206 @@
-import React, { useContext, useState, useEffect, useRef } from "react";
-import { Table, Input, Button, Popconfirm, Form } from "antd";
+import React, { useState } from "react";
+import { Table, Input, Typography, Popconfirm, Form, Button, Divider, Row, Col } from "antd";
+import { MinusOutlined } from "@ant-design/icons"
 
-const EditableContext = React.createContext(null);
-
-const EditableRow = ({ index, ...props }) => {
-  const [form] = Form.useForm();
+const EditableCell = ({
+  editing,
+  dataIndex,
+  title,
+  inputType,
+  record,
+  index,
+  children,
+  ...restProps
+}) => {
+  const inputNode = inputType === 'number' ? <InputNumber /> : <Input />;
   return (
-    <Form form={form} component={false}>
-      <EditableContext.Provider value={form}>
-        <tr {...props} />
-      </EditableContext.Provider>
-    </Form>
+    <td {...restProps}>
+      {editing ? (
+        <Form.Item
+          name={dataIndex}
+          style={{
+            margin: 0,
+          }}
+          rules={[
+            {
+              required: true,
+              message: `Introduzca ${title}!`,
+            },
+          ]}
+        >
+          {inputNode}
+        </Form.Item>
+      ) : (
+        children
+      )}
+    </td>
   );
 };
 
-const EditableCell = ({
-  title,
-  editable,
-  children,
-  dataIndex,
-  record,
-  handleSave,
-  ...restProps
-}) => {
-  const [editing, setEditing] = useState(false);
-  const inputRef = useRef(null);
-  const form = useContext(EditableContext);
-  useEffect(() => {
-    if (editing) {
-      inputRef.current.focus();
-    }
-  }, [editing]);
-
-  const toggleEdit = () => {
-    setEditing(!editing);
-    form.setFieldsValue({
-      [dataIndex]: record[dataIndex],
-    });
-  };
-
-  const save = async () => {
-    try {
-      const values = await form.validateFields();
-      toggleEdit();
-      handleSave({ ...record, ...values });
-    } catch (errInfo) {
-      console.log("Save failed:", errInfo);
-    }
-  };
-
-  let childNode = children;
-
-  if (editable) {
-    childNode = editing ? (
-      <Form.Item
-        style={{
-          margin: 0,
-        }}
-        name={dataIndex}
-        rules={[
-          {
-            required: true,
-            message: `${title} is required.`,
-          },
-        ]}
-      >
-        <Input ref={inputRef} onPressEnter={save} onBlur={save} />
-      </Form.Item>
-    ) : (
-      <div
-        className="editable-cell-value-wrap"
-        style={{
-          paddingRight: 24,
-        }}
-        onClick={toggleEdit}
-      >
-        {children}
-      </div>
-    );
-  }
-
-  return <td {...restProps}>{childNode}</td>;
-};
-
-
-
-const expandedRow = (dailyData, handleDelete) => {
-  const columns = [
-    {
-      title: "id",
-      dataIndex: "uid",
-    },
-    { title: 'Día', 
-      dataIndex: 'date', 
-      editable: true 
-    },
-    { title: 'Tipo de cultivo', 
-      dataIndex: 'crop_type', 
-      editable: true 
-    },
-    { title: 'Kilos', 
-      dataIndex: 'yield_values', 
-      editable: true 
-    },
-    {
-      title: "Operación",
-      dataIndex: "operation",
-      render: (_, record) =>
-        <Popconfirm
-          title="¿Está seguro?"
-          onConfirm={() => handleDelete(record.uid)}
-        >
-          <a>Delete</a>
-        </Popconfirm>
-    },
-  ];
-
-  const data = [];
-  for (let i = 0; i < dailyData.length; ++i) {
-    data.push({
-      uid: dailyData[i].uid,
-      date: dailyData[i].date,
-      crop_type: dailyData[i].crop_type,
-      yield_values: dailyData[i].yield_values,
-    });
-  }
-  return <Table columns={columns} dataSource={data} />;
-};
-
-
-
 const EditableTable = () => {
-  const tableColumns = [
-    {
-      title: "id",
-      dataIndex: "uid",
-    },
+  const [form] = Form.useForm();
+
+  // let n = (Math.random() * 0xfffff * 1000000).toString(16).slice(0, 8);
+  // fetchData = () => {
+  //   adalApiFetch(fetch, "/Tenant", {})
+  //     .then(response => response.json())
+  //     .then(responseJson => {
+  //       if (!this.isCancelled) {
+  //           const results= responseJson.map(row => ({
+  //               ClientId: row.ClientId,
+  //               ClientSecret: row.ClientSecret,
+  //               Id: row.Id,
+  //               SiteCollectionTestUrl: row.SiteCollectionTestUrl,
+  //               TenantDomainUrl: row.TenantDomainUrl
+  //             }))
+  //         this.setState({ data: results });
+  //       }
+  //     })
+  //     .catch(error => {
+  //       console.error(error);
+  //     });
+  // };
+
+  // The parent's keys shoud be equal to their corresponding date.
+  // The children's keys should be equal to their corresponding uid.
+  // https://stackoverflow.com/a/29813867/15612571
+  const [data, setData] = useState(
+    [
+      {
+        key: "2010-10",
+        date: "2010-10",
+        crop_type: "Mango Keitt",
+        yield_values: 14,
+        children: [
+          {
+            key: 1,
+            uid: 1,
+            date: "2010-10-14",
+            crop_type: "Mango Keitt",
+            yield_values: 9,
+          },
+          {
+            key: 2,
+            uid: 2,
+            date: "2010-10-23",
+            crop_type: "Mango Keitt",
+            yield_values: 5,
+          },          
+        ]
+      },
+      {
+        key: 15,
+        date: "2013-10",
+        crop_type: "Mango Tommy Atkins",
+        yield_values: 9,
+        children: [
+          {
+            key: 3,
+            uid: 3,
+            date: "2013-10-01",
+            crop_type: "Mango Tommy Atkins",
+            yield_values: 4,
+          },
+          {
+            key: 4,
+            uid: 4,
+            date: "2013-10-10",
+            crop_type: "Mango Tommy Atkins",
+            yield_values: 5,
+          },          
+        ]
+      },
+    ],
+  );
+
+  const [modifiedRows, setModifiedRows] = useState([]);
+  const [deletedRows, setDeletedRows] = useState([]);
+  const [editingKey, setEditingKey] = useState('');
+
+  const isEditing = (record) => record.key === editingKey;
+
+  const edit = (record) => {
+    console.log(modifiedRows);
+    form.setFieldsValue({
+      date: '',
+      crop_type: '',
+      yield_values: '',
+      ...record,
+    });
+
+    // The current row being edited is set by its key
+    setEditingKey(record.key);
+  };
+
+  const cancel = () => {
+    setEditingKey('');
+  };
+
+  const handleAdd = () => {
+    const newData = [...data];
+    const id = (Math.random() * 0xfffff * 1000000).toString(16).slice(0, 8);
+    const newRow = {
+      key: id,
+      uid: id,
+      date: "",
+      crop_type: "",
+      yield_values: "",
+    };
+    
+    setModifiedRows(modifiedRows.concat(newRow));
+
+    newData.push(newRow);
+    setData(newData);
+  };
+
+  const handleDelete = (key) => {
+    var newData = [...data];
+    const rowToDelete = newData.findIndex((item) => item.key === key);
+
+    if (rowToDelete != -1){
+      setDeletedRows(deletedRows.concat(newData[rowToDelete]));
+      newData = newData.filter((item) => item.key !== key);
+      setData(newData);
+    } else {
+      const index = newData.findIndex((item) => item.children.findIndex((child) => key === child.key) > -1);
+      const indexChild = newData[index].children.findIndex((child) => key === child.key);
+      setDeletedRows(deletedRows.concat(newData[index].children[indexChild]));
+      newData[index].children.splice(indexChild, 1);
+      setData(newData);
+    }
+    
+  };
+
+  const handleSave = async (key) => {
+    try {
+      const row = await form.validateFields();
+      const newData = [...data];
+      const rowToSave = newData.findIndex((item) => item.key === key);
+      
+      if (rowToSave != -1){
+        newData.splice(rowToSave, 1, {...newData[rowToSave], ...row});
+        setModifiedRows(modifiedRows.concat(newData[rowToSave]));
+      } else {
+        const index = newData.findIndex((item) => item.children.findIndex((child) => key === child.key) > -1);
+
+        const indexChild = newData[index].children.findIndex((child) => key === child.key);
+        const itemChildren = newData[index].children;
+        itemChildren.splice(indexChild, 1, { ...itemChildren[indexChild], ...row });
+
+        setModifiedRows(modifiedRows.concat(itemChildren[indexChild]));
+      }
+      setData(newData);
+      setEditingKey('');       
+    } catch (errInfo) {
+      console.log('Validate Failed:', errInfo);
+    }
+  };
+
+  const handleCommit = async () => {
+    
+  }
+
+  const columns = [
     {
       title: "Fecha",
       dataIndex: "date",
@@ -151,97 +216,46 @@ const EditableTable = () => {
       dataIndex: "yield_values",
       editable: true,
     },
-  ];
-  const [state, setState] = useState({
-    monthlyData: [
-      {
-        uid: 1,
-        date: "2010-10",
-        crop_type: "Mango Keitt",
-        yield_values: 14,
+    {
+      title: 'Operación',
+      dataIndex: 'operation',
+      render: (_, record) => {
+        if (record.children){
+          return (
+            <MinusOutlined/>
+          )
+        }
+        const editable = isEditing(record);
+        return editable ? (
+          <span>
+            <a
+              href="javascript:void(0)"
+              onClick={() => handleSave(record.key)}
+              style={{
+                marginRight: 8,
+              }}
+            >
+              Guardar
+            </a>
+            <Popconfirm title="¿Desea cancelar?" onConfirm={cancel}>
+              <a>Cancelar</a>
+            </Popconfirm>
+          </span>
+        ) : (
+          <>
+          <Typography.Link disabled={editingKey !== ''} onClick={() => edit(record)}>
+            Editar
+          </Typography.Link>
+          <Divider type="vertical"/>
+          <Popconfirm title="¿Desea borrar la fila?" onConfirm={() => handleDelete(record.key)}>
+              <a>Eliminar</a>
+          </Popconfirm>
+          </>
+        );
       },
-      {
-        uid: 2,
-        date: "2013-10",
-        crop_type: "Mango Tommy Atkins",
-        yield_values: 9,
-      },
-    ],
-    dailyData: [
-      {
-        uid: 1,
-        date: "2010-10-14",
-        crop_type: "Mango Keitt",
-        yield_values: 9,
-      },
-      {
-        uid: 2,
-        date: "2010-10-23",
-        crop_type: "Mango Keitt",
-        yield_values: 5,
-      },
-      {
-        uid: 3,
-        date: "2013-10-01",
-        crop_type: "Mango Tommy Atkins",
-        yield_values: 4,
-      },
-      {
-        uid: 4,
-        date: "2013-10-10",
-        crop_type: "Mango Tommy Atkins",
-        yield_values: 5,
-      },
-    ],
-    count: 4,
-  });
-
-  const handleDelete = (uid) => {
-    const { count, dataSource } = state;
-    setState({
-      dataSource: dataSource.filter((item) => item.uid !== uid),
-      count: count - 1,
-    });
-  };
-
-  const handleAdd = () => {
-    const { count, dataSource } = state;
-    const newData = {
-      uid: count + 1,
-      date: "",
-      crop_type: "",
-      yield_values: 0,
-    };
-    setState({
-      dataSource: [...dataSource, newData],
-      count: count + 1,
-    });
-  };
-
-  const handleSaveCell = (row) => {
-    const newData = [...state.dataSource];
-    const index = newData.findIndex((item) => row.uid === item.uid);
-    const item = newData[index];
-    newData.splice(index, 1, { ...item, ...row });
-    setState({
-      dataSource: newData,
-    });
-  };
-
-  const handleSaveToDB = () => {
-    // TODO
-  };
-
-  const dataSourceMontly = state.monthlyData;
-  const dataSourceDaily = expandedRow(state.dailyData, handleDelete);
-  const components = {
-    body: {
-      row: EditableRow,
-      cell: EditableCell,
     },
-  };
-
-  const columns = tableColumns.map((col) => {
+  ];
+  const mergedColumns = columns.map((col) => {
     if (!col.editable) {
       return col;
     }
@@ -250,35 +264,57 @@ const EditableTable = () => {
       ...col,
       onCell: (record) => ({
         record,
-        editable: col.editable,
+        inputType: col.dataIndex === 'date' ? 'crop_type' : 'yield_values',
         dataIndex: col.dataIndex,
         title: col.title,
-        handleSave: handleSaveCell,
+        editing: isEditing(record),
       }),
     };
   });
-
   return (
-    <>
-      <Button
-        onClick={handleAdd}
-        type="primary"
-        style={{
-          marginBottom: 16,
-        }}
-      >
-        Añadir una entrada
-      </Button>
+    <Form form={form} component={false}>
+        <Row justify="space-between">
+          <Col>
+            <Button
+              onClick={handleAdd}
+              type="primary"
+              style={{
+                marginBottom: 16,
+              }}
+            >
+              Añadir fila
+            </Button>
+          </Col>
+          <Col>
+            <Button
+              onClick={handleCommit}
+              type="primary"
+              style={{
+                marginBottom: 16,
+              }}
+            >
+              Aplicar cambios
+            </Button>
+          </Col>
+        </Row>
+
       <Table
-        components={components}
-        rowClassName={() => "editable-row"}
+        components={{
+          body: {
+            cell: EditableCell,
+          },
+        }}
         bordered
-        expandable={{ dataSourceDaily }}
-        dataSource={dataSourceMontly}
-        columns={columns}
+        dataSource={data}
+        columns={mergedColumns}
+        rowClassName="editable-row"
+        pagination={{
+          onChange: cancel,
+        }}
       />
-    </>
+    </Form>
   );
 };
+
 
 export default EditableTable;
